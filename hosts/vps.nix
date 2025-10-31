8149babc9f31b241ec2f2509fe5e18b48f1c3c75
matty <github@doppel.moe>
2025-10-31 10:02:04 -0400

    (hosts) move stuff to common.nix

--- hosts/vps.nix
+++ hosts/vps.nix
@@ -8,35 +8,14 @@
   environment.systemPackages = with pkgs; [
     nano
     git
+    wget
   ];
   
   time.timeZone = "Europe/Berlin";
   i18n.defaultLocale = "en_US.UTF-8";
   console.keyMap = "us";
   
-  users.users = {
-    root.hashedPassword = "!"; # Disable root login
-    sana = {
-      isNormalUser = true;
-      extraGroups = [ "wheel" ];
-      openssh.authorizedKeys.keys = [
-        ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBLd2Cxrph6ES6ClDbqXFDWhI9+N+2+sKI5DMlFOco0XMmX8kHUNEOZengOpzJQ60EcNYYZbBd6mapqAd3Z/bGQg= sana@doppel.moe
-      ];
-    };
-  };
-  
-  security.sudo.wheelNeedsPassword = false;
-  
-  services.openssh = {
-    enable = true;
-    settings = {
-      PermitRootLogin = "no";
-      PasswordAuthentication = false;
-      KbdInteractiveAuthentication = false;
-    };
-  };
-  
   networking.firewall.allowedTCPPorts = [ 22 ];
   
-  system.stateVersion = "25.05";
+  system.stateVersion = "25.11";
 }
