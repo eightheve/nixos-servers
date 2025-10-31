2e36004fe4ef7819f1a1f7e57ff303be1c05fe63
matty <github@doppel.moe>
2025-10-31 11:14:38 -0400

    change hostname

--- hosts/vps.nix
+++ hosts/vps.nix
@@ -15,7 +15,8 @@
   i18n.defaultLocale = "en_US.UTF-8";
   console.keyMap = "us";
   
-  networking.firewall.allowedTCPPorts = [ 22 ];
+  networking.firewall.allowedTCPPorts = [ 22 2222 ];
+  networking.hostName = "KAZOOIE";
   
   system.stateVersion = "25.11";
 }
