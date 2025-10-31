926b1b19e46dddda35eea19e0f02d00ba6eda725
matty <github@doppel.moe>
2025-10-31 11:18:19 -0400

    remove sana from thinkcenter config (redundant)

--- hosts/thinkcenter.nix
+++ hosts/thinkcenter.nix
@@ -20,14 +20,6 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   users.users = {
-    sana = {
-      isNormalUser = true;
-      extraGroups = [ "wheel" ];
-      openssh.authorizedKeys.keys = [
-        "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBLd2Cxrph6ES6ClDbqXFDWhI9+N+2+sKI5DMlFOco0XMmX8kHUNEOZengOpzJQ60EcNYYZbBd6mapqAd3Z/bGQg= sana"
-        "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAGXE85j6/DEbt7UZS2nRfjNzdPClPwBG/admRewhTYUnr2JeXr+lLOS9tWk/vsgzkwFaxvlUvvRvTWWJwoeZd7DnwE2oy+1GXwFPqUHjDQg/DpQR8eu7U28Do6+moNTqLGW13FrMyv4CraXTcSn40E5PZvuOWyNjhsGSsFHeyt/J9Fgjg== sana"
-      ];
-    };
     nxsu = {
       isNormalUser = true;
       #extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
